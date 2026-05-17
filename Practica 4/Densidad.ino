5a8408f0d635bd8e27234334bcbe90c77de734da
valecarrher <valecarrher@gmail.com>
2026-05-17 12:49:05 -0600

    Update Densidad.ino

--- Practica 4/Densidad.ino
+++ Practica 4/Densidad.ino
@@ -1,1 +1,35 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
 
+Adafruit_BMP085 bmp;
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!bmp.begin()) {
+    Serial.println("No se encontró BMP180");
+    while (1);
+  }
+}
+
+void loop() {
+
+  float temperatura = bmp.readTemperature();
+  float presion = bmp.readPressure();
+
+  // densidad aproximada
+  float R = 287.05;
+
+  float densidad = presion / (R * (temperatura + 273.15));
+
+  Serial.print("Temperatura: ");
+  Serial.println(temperatura);
+
+  Serial.print("Presion: ");
+  Serial.println(presion);
+
+  Serial.print("Densidad: ");
+  Serial.println(densidad);
+
+  delay(2000);
+}
